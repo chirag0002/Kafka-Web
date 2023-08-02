8dd613156037ab266f8518acd97585b99177bb79
chirag0002 <varshneychirag34@gmail.com>
2023-08-02 13:08:34 +0530

    staging files

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use rdkafka::config::ClientConfig;
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use rocket::State;
 use rdkafka::util::Timeout;
+use rdkafka::consumer::{BaseConsumer, Consumer};
+use rdkafka::message::Message;
+
+
 
 #[get("/")]
 fn index() -> &'static str {
@@ -13,8 +17,8 @@ fn index() -> &'static str {
 
 #[get("/button1")]
 async fn button1(producer: &State<FutureProducer>) -> &'static str {
-    let topic = "sample";
-    let value = "Hii!";
+    let topic = "topic-1";
+    let value = "Button 1 Clicked !!!";
     let record = FutureRecord::to(topic).payload(value).key("button1");
     let timeout = Timeout::Never;
     
@@ -24,7 +28,30 @@ async fn button1(producer: &State<FutureProducer>) -> &'static str {
 }
 
 #[get("/button2")]
-async fn button2() -> &'static str {
+async fn button2(consumer: &State<BaseConsumer>) -> &'static str {
+    let timeout = std::time::Duration::from_secs(5);
+    consumer
+        .subscribe(&["topic-1"])
+        .expect("Failed to subscribe to topic");
+     
+
+    loop {
+        match consumer.poll(timeout) {
+            Some(Ok(message)) => {
+                if let Some(payload) = message.payload_view::<str>() {
+                    println!("Received message: {}", payload.unwrap());
+                }
+            }
+            Some(Err(e)) => {
+                eprintln!("Error while receiving message: {:?}", e);
+            }
+            None => {
+                println!("No Received message");
+                break;
+            }
+        }
+    }
+
     "This is the second button."
 }
 
@@ -35,10 +62,16 @@ fn rocket() -> _ {
         .set("bootstrap.servers", "localhost:9092")
         .create::<FutureProducer>()
         .expect("Failed to create producer");
+ 
+      
+    let consumer: BaseConsumer = ClientConfig::new()
+        .set("bootstrap.servers", "localhost:9092")
+        .set("group.id", "my-consumer-group")
+        .create()
+        .expect("Failed to create consumer");
 
-    
     rocket::build()
         .manage(producer)
-        // .manage(consumer)
+        .manage(consumer)
         .mount("/", routes![index, button1, button2])
 }
